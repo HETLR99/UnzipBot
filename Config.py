587724d1315a01bee8e319c108be01e5fa299219
HETLR99 <x.x.f.f.f.0@gmail.com>
2024-08-12 12:58:49 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 20077058))  # Change 12345 to your API_ID
-    API_HASH = os.environ.get("API_HASH", "349639880eb5b1cb0ff6c2b8a6f16717")  # Change None to your API_HASH
+    API_ID = int(os.environ.get("API_ID", 16536417))  # Change 12345 to your API_ID
+    API_HASH = os.environ.get("API_HASH", "f6e58a549da642d7b765744a2f82c6d9")  # Change None to your API_HASH
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7330436352:AAHUJUNGytd9bTY1otHe7EVwJlN4km0usXw")  # Change None to your BOT_TOKEN
     OWNER_ID = int(os.environ.get("OWNER_ID", 6169288210))  # Change 0 to your OWNER_ID
     OWNER_NAME = os.environ.get("OWNER_NAME", "ؖؖؖؖؖؖؖؖؖ𓆪:ᵛ͢ᵎᵖ ┋ﷺ آلُـِـِِـِِِـِِـِـﮩرمـْـْْـْ آلُـِـِِـِِِـِِـِـڛـ,ـآبـٌـٌٌـٌٌٌـٌٌـٌعٌ ﷺ┋[ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩ")  # Change None to your OWNER_NAME
